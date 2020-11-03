f1e41b8c9c073d2d3c7b0a587b30d75b03b3dc98
nminh227 <55972163+nminh227@users.noreply.github.com>
2020-11-03 20:53:39 +0700

    Add files via upload

---
+++ OnTap_01/T3/DNMinh_LeetCode_1089.java
@@ -0,0 +1,13 @@
+package vn.techmaster;
+
+public class duplicateZeros {
+    public void duplicateZ(int[] arr) {
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == 0){
+                for (int j = arr.length-1; j > i; j--){
+                    arr[j] = arr[j-1];}
+                i++;
+            }
+        }
+    }
+}
---
+++ OnTap_01/T3/DNMinh_LeetCode_1925.java
@@ -0,0 +1,12 @@
+package vn.techmaster;
+
+public class evenNOD {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for(int i:nums){
+            if(String.valueOf(i).length() % 2 == 0)
+                count++;
+        }
+        return count;
+    }
+}
