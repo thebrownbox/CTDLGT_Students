3d07f9df34bbf4951749d86256db25cb052cd963
legianha <doantrihung592@gmail.com>
2020-11-04 08:32:22 +0700

    add java

---
+++ OnTap_01/T7/Nha_70_C1.java
@@ -0,0 +1,23 @@
+class Solution {
+    int count = 0;
+    
+    public void recursion(int[] arr, int index, int currentStep, int totalStep){
+        if (currentStep >= totalStep){
+            if (currentStep == totalStep){
+                count++;
+            }
+            return;
+        }
+        
+        for (int i=1; i<=2; i++){
+            arr[index] = i;
+            recursion(arr, index+1, currentStep+i, totalStep);
+        }
+    }
+    
+    public int climbStairs(int n) {
+        int[] stepArr = new int[n];
+        recursion(stepArr, 0, 0, n);
+        return count;
+    }
+}
