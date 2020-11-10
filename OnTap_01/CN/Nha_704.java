590b7b98a09a05920454f61f11ac4eabe2c4ff59
legianha <doantrihung592@gmail.com>
2020-11-10 19:12:28 +0700

    add java

--- OnTap_01/CN/Nha_704.java
+++ OnTap_01/CN/Nha_704.java
@@ -2,21 +2,16 @@ class Solution {
     public int search(int[] nums, int target) {
         int first = 0, last = nums.length-1;
         while(true){
-            if (first == last){
-                if (nums[first] == target){
-                    return first;
-                }
-                else{
-                    return -1;
-                }
+            if (first > last){
+                return -1;
             }
             else{
-                int middle = (first + last)/2;
+                int middle = (first+last)/2;
                 if (nums[middle] == target){
                     return middle;
                 }
                 else if (nums[middle] > target){
-                    last = middle;
+                    last = middle - 1;
                 }
                 else{
                     first = middle + 1;
