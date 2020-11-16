4d5be90399357d18c167d746af0e48831b2a5f40
cuong <chuchicuong>
2020-11-16 20:00:46 +0700

    cuong

---
+++ Lesson_08_StackQueue/CCuong232.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.Stack;
+
+//letcode 232
+public class MyQueue {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+    /** Initialize your data structure here. */
+    public MyQueue() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (!stack2.isEmpty()){
+            return stack2.pop();
+        }else
+            while (!stack1.isEmpty())
+                return stack2.push(stack1.pop());
+            return stack2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (!stack2.isEmpty()){
+            return stack2.peek();
+        }else
+            while (!stack1.isEmpty())
+                return stack2.push(stack1.pop());
+        return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.empty() && stack2.empty();
+    }
+
+    public static void main(String[] args) {
+        MyQueue m = new MyQueue();
+        m.push(2);
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
