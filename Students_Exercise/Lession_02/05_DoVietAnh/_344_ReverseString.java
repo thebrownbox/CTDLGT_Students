7b83dff130883e4f94d949d9cbe8793d12ad048a
DESKTOP-Q6N4J46\Admin <vietanhanhviet@gmail.com>
2021-04-02 09:15:07 +0700

    Do Viet Anh nop bai

---
+++ Students_Exercise/Lession_02/05_DoVietAnh/_344_ReverseString.java
@@ -0,0 +1,33 @@
+package anhdv;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author anhdv
+ * Write a function that reverses a string. The input string is given as an array of characters
+ */
+class _344_ReverseString {
+
+    static void reverseString(char[] s) {
+        int j = s.length - 1;
+        char temp;
+        for( int i = 0; i < s.length; ) {
+            if( i >= j )
+                break;
+            
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            
+            i++;
+            j--;
+        }
+    }
+    
+    public static void main(String[] args) {
+        char[] arr = {'a', 'b', 'c', 'd', 'e', 'f'};
+        reverseString(arr);
+        System.out.println("result: " + Arrays.toString(arr));
+    }
+}
