a451a0fa126f7dc25fe6734e314388aa217e4a41
buiduong2 <buiducduong1@gmail.com>
2021-04-08 23:16:57 +0700

    Dương sửa nhẹ Lession_04

--- Students_Exercise/Lession_04/13_BuiDucDuong/_704_BinarySearch.java
+++ Students_Exercise/Lession_04/13_BuiDucDuong/_704_BinarySearch.java
@@ -8,20 +8,23 @@ public class _704_BinarySearch {
     }
 
     public static int search(int[] nums, int target) {
-        return search(nums, target, 0, nums.length - 1);
-    }
-
-    private static int search(int[] nums, int target, int l, int r) {
-        if (nums[l] <= target && nums[r] >= target) {
-            int val = (r - l) * (target - nums[l]) / (nums[r] - nums[l]) + l;
-            if (nums[val] < target) {
-                return search(nums, target, val + 1, r);
-            } else if (nums[val] > target) {
-                return search(nums, target, l, val - 1);
-            } else {
-                return val;
-            }
+        if(nums.length ==1) {
+            return nums[0] == target ? 0 : -1;
+        } 
+            return search(nums, target, 0, nums.length - 1);
         }
-        return -1;
-    }
+    
+        private static int search(int[] nums, int target, int l, int r) {
+            if (nums[l] <= target && nums[r] >= target) {
+                int val = (r - l) * (target - nums[l]) / (nums[r] - nums[l]) + l;
+                if (nums[val] < target) {
+                    return search(nums, target, val + 1, r);
+                } else if (nums[val] > target) {
+                    return search(nums, target, l, val - 1);
+                } else {
+                    return val;
+                }
+            }
+            return -1;
+        }
 }
