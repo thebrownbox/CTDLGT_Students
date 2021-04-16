c0b0b740b33958f84f74d402849ecddd65a10e43
hanhtd161 <hanhtd161@gmail.com>
2021-04-16 15:14:37 +0700

    bai6

---
+++ Students_Exercise/Lession_06/06_TranDucHanh/_203_RemoveLinkedListElements.java
@@ -0,0 +1,20 @@
+public class _203_RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        if (head == null) {
+            return null;
+        }
+        ListNode temp = head;
+        while (temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+
+        }
+        return head;
+    }
+}
