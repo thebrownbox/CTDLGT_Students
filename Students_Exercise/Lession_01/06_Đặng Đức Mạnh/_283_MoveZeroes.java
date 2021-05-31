81705a21409380551d6b5a9c2fd7ec1c3652ba1b
Duc Manh <ducmanhtb2802@gmail.com>
2021-05-31 09:40:59 +0700

    First commit

---
+++ Students_Exercise/Lession_01/06_Đặng Đức Mạnh/_283_MoveZeroes.java
@@ -0,0 +1,8 @@
+//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+public class _283_MoveZeroes {
+    public static void moveZero(int[] nums){
+        
+    }
+    
+}
