13ee4dcf12ccb752437ce93feb7d4a4f737dca3d
duy nguyen <thoduyqt@gmail.com>
2021-06-01 17:38:34 +0700

    update exercise

---
+++ Students_Exercise/Lession_02/02_Nguyễn Thọ Duy/_283_Move_Zeroes.java
@@ -0,0 +1,14 @@
+public class _283_Move_Zeroes {
+        class Solution {
+                public void moveZeroes(int[] arr) {
+                     int n = arr.length;
+                            for (int i = 0; i < n - 1; i++)
+                                for (int j = 0; j < n - i - 1; j++)
+                                    if (arr[j] == 0) {
+                                        int temp = arr[j];
+                                        arr[j] = arr[j + 1];
+                                        arr[j + 1] = temp;
+                            }
+                }
+            }
+}
