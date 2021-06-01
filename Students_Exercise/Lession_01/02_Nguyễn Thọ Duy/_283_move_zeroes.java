616fc03b268499c5218995e3b91ffa3c080fd961
duy nguyen <thoduyqt@gmail.com>
2021-06-01 23:22:17 +0700

    push exrcise lap 01

---
+++ Students_Exercise/Lession_01/02_Nguyễn Thọ Duy/_283_move_zeroes.java
@@ -0,0 +1,19 @@
+public class _283_move_zeroes {
+        public static void moveZeroes(int[] arr) {
+                int n = arr.length;
+                       for (int i = 0; i < n - 1; i++)
+                           for (int j = 0; j < n - i - 1; j++)
+                               if (arr[j] == 0) {
+                                   int temp = arr[j];
+                                   arr[j] = arr[j + 1];
+                                   arr[j + 1] = temp;
+                       }
+           }
+           public static void main(String[] args) {
+                int[] nums = { 0,2,5,0,1};
+
+               moveZeroes(nums);
+
+               System.out.println("check success!");
+           }
+}
