f60c568c87e218bdc82622ad28959d21f8efd639
Tho Duy <thoduy@Thos-MacBook-Pro.local>
2021-06-11 23:02:36 +0700

    push exercise ls 4

---
+++ Students_Exercise/Lession_04/02_Nguyễn Thọ Duy/MathArray.java
@@ -0,0 +1,18 @@
+
+public class MathArray {
+    //Implement hàm sum1 để tính tổng các phần tử có trong mảng a
+    private static int sum1(int a[], int n) {
+        if (n == a.length) {
+            return 0;
+        }
+        return sum1(a, n + 1) + a[n];
+    }
+
+    public static void main(String[] args) {
+        int a[] = { 1, 2, 5, 6, 7, 9, 10 };
+        int sum = sum1(a, 0);
+
+        System.out.println(sum);
+    }
+
+}
