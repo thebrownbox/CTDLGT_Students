d186f81700ac4ea80312fc9492f63f6193930556
Tin1003 <77330928+Tin1003@users.noreply.github.com>
2021-06-12 19:36:11 +0700

    em tin nop bai

---
+++ Students_Exercise/Lession_03/05_Phạm Hữu Tín/_1051_Height_Checker.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+public class _1051_Height_Checker {
+    public int heightChecker(int[] heights) {
+        
+        int n = heights.length;
+        int[] copied = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            copied[i] = heights[i];
+        }
+//        printArray(copied);
+
+        // selection sort
+        for (int i = 0; i < n; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if(copied[j] < copied[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+            if(minIndex != i)
+            {
+                int t = copied[i];
+                copied[i] = copied[minIndex];
+                copied[minIndex] = t;
+            }
+
+        }
+
+//        printArray(copied);
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if(heights[i] != copied[i])
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,1,4,2,1,3};
+        System.out.println(heightChecker(arr));
+    }
+    
+    
+}
