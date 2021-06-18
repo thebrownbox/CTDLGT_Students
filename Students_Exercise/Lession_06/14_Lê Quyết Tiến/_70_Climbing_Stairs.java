55ad90eea7a9d03041e752825018aadbcfaf3e2e
Tien <lequyettien93@gmail.com>
2021-06-18 11:07:29 +0700

    hw6

---
+++ Students_Exercise/Lession_06/14_Lê Quyết Tiến/_70_Climbing_Stairs.java
@@ -0,0 +1,27 @@
+public class _70_Climbing_Stairs {
+
+    static int[] step = new int[45];
+
+    public static int climb(int n) {
+        if (step[n] != 0) {
+            return step[n];
+        }
+        step[n] = climb(n - 1) + climb(n - 2);
+        return step[n];
+    }
+    
+    public static int climbStairs(int n) {
+        int result = 0;
+        step[1] = 1;
+        step[2] = 2;
+        if (n < 1 || n > 45) {
+            result = 0;
+        } else {
+            result = climb(n);
+        }
+        return result;
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println(climbStairs(7));
+    }
+}
