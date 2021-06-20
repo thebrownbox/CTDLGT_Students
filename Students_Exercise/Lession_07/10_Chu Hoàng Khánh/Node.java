d25a41bae0e5214f2014519bc06061f2dbfc441c
hoangkhanh2208 <chuhoangkhanh00@gmail.com>
2021-06-20 22:21:10 +0700

    submit

--- Students_Exercise/Lession_07/10_Chu Hoàng Khánh/Node.java
+++ Students_Exercise/Lession_07/10_Chu Hoàng Khánh/Node.java
@@ -3,8 +3,15 @@ public class Node {
     int val;
     Node next;
 
-    public Node(int val){
-        this.next = null;
+    public Node(){}
+
+    public Node(int val) {
         this.val = val;
     }
+    
+    public Node(int val, Node next) {
+        this.val = val;
+        this.next = next;
+    }
+    
 }
---
+++ Students_Exercise/Lession_07/10_Chu Hoàng Khánh/_206_Reverse_Linked_List.java
@@ -0,0 +1,14 @@
+public class _206_Reverse_Linked_List {
+    public static void main(String[] args) {
+
+    }
+    
+    public Node reverse(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.next == null)
+            return node;
+        return null;
+    }
+}
