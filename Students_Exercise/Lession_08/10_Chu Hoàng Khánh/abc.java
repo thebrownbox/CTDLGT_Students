7ce3b415fd55f8f2e888628f3e3e2f83c3943b1c
hoangkhanh2208 <chuhoangkhanh00@gmail.com>
2021-06-24 21:55:26 +0700

    submit

---
+++ Students_Exercise/Lession_08/10_Chu Hoàng Khánh/abc.java
@@ -0,0 +1,9 @@
+public class abc {
+    public static void main(String[] args) {
+        String s = "leEeetcode";
+
+        char c = s.charAt(1);
+        int a = Math.abs(c - s.charAt(2));
+        System.out.println( a);
+    }
+}
