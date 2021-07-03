0a6c8f9a97addd47dccfe927de4bf6f7eb39cfaf
Tho Duy <thoduy@Thos-MacBook-Pro.local>
2021-07-03 17:23:42 +0700

    push exercise ls10

---
+++ Students_Exercise/Lession_08/02_Nguyễn Thọ Duy/_1046_Last_Stone_Weight.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _1046_Last_Stone_Weight {
+    public static int lastStoneWeight(int[] stones) {
+	   Queue<Integer> myQueue = new LinkedList<>(); 
+
+		for (int stone : stones) {
+			myQueue.add(stone);
+		}
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        //chua co y tuowng lam bai nay!
+    }
+}
